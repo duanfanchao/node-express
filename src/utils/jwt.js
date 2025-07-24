4c5b2394e9314ba543898bbf94ec4b6129439c00
duan150385duan <Lebron150385@163.com>
2025-07-24 14:06:17 +0800

    解决token过期未正确提示的问题

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,13 +2,8 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET;
 
 // 生成Token（1分钟过期）
-exports.generateToken = (payload, expiresIn = '1m') => {
+exports.generateToken = (payload, expiresIn = '5s') => {
     return jwt.sign(payload, secret, { expiresIn });
-};
-
-// 生成Refresh Token（7天过期）
-exports.generateRefreshToken = (payload) => {
-    return jwt.sign(payload, secret + '_REFRESH', { expiresIn: '7d' });
 };
 
 // 验证Token
